955ac0c311a0910c510535d90fb9cbbc378c3f09
Bamba9016 <issa.bamba.0201@gmail.com>
2025-04-04 03:12:33 +0200

    Update settings.py

--- ledjassa/settings.py
+++ ledjassa/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djassa',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'ton_mot_de_passe',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
