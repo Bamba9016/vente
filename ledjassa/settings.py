a9f9621f7d699791f80f6005bf0a643603927560
Bamba9016 <issa.bamba.0201@gmail.com>
2025-04-04 04:41:58 +0200

    Update settings.py

--- ledjassa/settings.py
+++ ledjassa/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ledjassa.wsgi.application'
 DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djassar',
+        'NAME': 'djassa',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
